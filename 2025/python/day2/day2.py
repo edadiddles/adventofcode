2d03d923679343f16dd136d320b406b6577d59c9
Edward Ruzek <edadiddles@hotmail.com>
2025-12-02 09:02:49 -0500

    first attempt at day2 part 2. using a naive solution

--- 2025/python/day2/day2.py
+++ 2025/python/day2/day2.py
@@ -11,9 +11,25 @@ for datum in data:
     [low, high] = datum.split("-")
     print(f"{low} - {high}")
     while int(low) <= int(high):
-        if low[:len(low)//2] == low[len(low)//2:]:
-            print(f"duplicated product number: {low}")
-            invalid_prod_nums.append(int(low))
+        l = 1
+        while l <= len(low)//2:
+            s = len(low)//l
+            if s != len(low)/l:
+                l += 1
+                continue
+
+            is_equal = True
+            for k in range(1, s):
+                if low[(k-1)*l:k*l] != low[k*l:(k+1)*l]:
+                    is_equal = False
+                    break
+
+            if is_equal:
+                print(f"duplicated product number: {low}")
+                invalid_prod_nums.append(int(low))
+                break
+
+            l += 1
         low = str(int(low)+1)
 
 print("sum of invalid product numbers:", sum(invalid_prod_nums))

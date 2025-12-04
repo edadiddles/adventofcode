a0d13dd307b62efe505e92bb00475f7f85df0529
Edward Ruzek <edadiddles@hotmail.com>
2025-12-04 09:15:03 -0500

    day2 part2 completed

--- 2025/python/day4/day4.py
+++ 2025/python/day4/day4.py
@@ -1,4 +1,5 @@
 import sys
+from time import sleep
 
 filename = sys.argv[1]
 
@@ -8,27 +9,36 @@ def run(filename):
         rows = f.read().strip().split("\n")
 
     dirs = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
-    num_access = 0
+    total_access = 0
     output = []
-    for j, row in enumerate(rows):
-        output_row = []
-        for i, col in enumerate(row):
-            output_row.append(col)
-            num_adj = 0
-            if col == "@":
-                for dir in dirs:
-                    x, y = dir
-                    if x+j >= 0 and x+j < len(row) and y+i >= 0 and y+i < len(rows) and rows[x+j][y+i] == "@":
-                        num_adj += 1
+    input = rows
+    num_access = 10  # hack to get into loop
+    while num_access > 0:
+        num_access = 0
+        for j, row in enumerate(input):
+            #print(row)
+            output_row = []
+            for i, col in enumerate(row):
+                output_row.append(col)
+                num_adj = 0
+                if col == "@":
+                    for dir in dirs:
+                        x, y = dir
+                        if x+j >= 0 and x+j < len(row) and y+i >= 0 and y+i < len(input) and input[x+j][y+i] == "@":
+                            num_adj += 1
 
-                if num_adj < 4:
-                    output_row[-1] = "x"
-                    num_access += 1
+                    if num_adj < 4:
+                        output_row[-1] = "x"
+                        num_access += 1
 
-        output.append("".join(output_row))
+            output.append("".join(output_row))
 
-    print(f"Output: {num_access}")
-    print("\n".join(output))
+        total_access += num_access
+        input = output
+        output = []
+        #print("----------")
+
+    print(f"Output: {total_access}")
 
 
 run(filename)
